59918ff3bb4558c2250eb8c19699c3ff5ca2f6a8
= <david@alwaysmovefast.com>
2011-05-16 13:52:31 -0700

    simplified intreverse ops

--- c/array_manipulation/intrev.c
+++ c/array_manipulation/intrev.c
@@ -43,10 +43,9 @@ int do_intrev(int argc, char **argv) {
 static int intreverse(int n) {
     int r = 0;
     while (n) {
+        r *= 10;
         r += n % 10;
         n /= 10;
-        if (n)
-            r *= 10;
     }
     return r;
 }
