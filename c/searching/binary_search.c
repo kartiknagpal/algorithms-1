113e9d700e7eaeadddd5328e544d2151890ed772
David Reynolds <david@alwaysmovefast.com>
2013-03-04 09:18:44 -0800

    fixing issue #8 from michael105

--- c/searching/binary_search.c
+++ c/searching/binary_search.c
@@ -14,12 +14,12 @@ int binary_search(int A[], int p, int r, int v) {
         /* search right */
         return binary_search(A, m+1, r, v);
     }
-    return binary_search(A, p, m-1, v);
+    return binary_search(A, p, m, v);
 }
 
 int main(int argc, char **argv) {
     int A[8] = {1, 2, 3, 4, 5, 6, 7, 8};
-    int res = binary_search(A, 0, 8, 0);
+    int res = binary_search(A, 0, 8, 8);
 
     if (res >= 0) {
         printf("A[%d] = %d\n", res, A[res]);
